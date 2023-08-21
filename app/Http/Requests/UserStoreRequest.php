6a759d8ce2b0d4c38261c1dd9a094de840c6db87
Mariya Villamor <mariyavillamor@gmail.com>
2023-08-21 21:23:44 +0800

    added: profile picture and cover photo upload functionality

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -37,6 +37,6 @@ class UserStoreRequest extends FormRequest
             'success' => false,
             'message' => 'Validation errors',
             'data' => $validator->errors(),
-        ]));
+        ],400));
     }
 }
